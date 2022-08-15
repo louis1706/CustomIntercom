3c4fb51be7ff0c8fafc888f0f1a87893be4a5c95
louis1706 <louismonneyron5@yahoo.com>
2022-08-15 14:40:24 +0200

    Initial commit

---
+++ IntercomPatche.cs
@@ -0,0 +1,24 @@
+﻿using HarmonyLib;
+using System;
+using Exiled.API.Features;
+
+namespace CustomIntercom
+{
+    [HarmonyPatch(typeof(Intercom), nameof(Intercom.UpdateText))]
+
+    public class IntercomUpdateTextPatches
+    {
+        public static void Prefix(Intercom __instance)
+        {
+            try
+            {
+                __instance.CustomContent = ServerConsole.singleton.NameFormatter.ProcessExpression(IntercomTextUpdate.Singleton.Config.CustomText);
+            }
+            catch (Exception ex)
+            {
+                Log.Error($"Intercom::UpdateText Prefix : {ex}\n {ex.StackTrace}");
+            }
+        }
+    }
+}
+
---
+++ Main.cs
@@ -0,0 +1,42 @@
+﻿namespace CustomIntercom
+{
+	using Exiled.API.Enums;
+	using Exiled.API.Features;
+    using HarmonyLib;
+    using System;
+
+    public class IntercomTextUpdate : Plugin<Config>
+	{
+		public static IntercomTextUpdate Singleton;
+
+		public override string Name => "CustomIntercom";
+		public override string Prefix => "CustomIntercom";
+		public override string Author => "Yamato#8987";
+
+		private Harmony Harmony = new("dev.Yamato");
+		private int patchCounter;
+
+		public override void OnEnabled()
+		{
+			Singleton = this;
+
+			try
+			{
+				Harmony = new(Author + "." + Name + ++patchCounter);
+				Harmony.PatchAll();
+			}
+			catch (Exception ex)
+			{
+				Log.Error($"[RegistPatch] Patching Failed : {ex}");
+			}
+			base.OnEnabled();
+		}
+		public override void OnDisabled()
+		{
+			Harmony.UnpatchAll(Harmony.Id);
+
+			base.OnDisabled();
+		}
+
+	}
+}
