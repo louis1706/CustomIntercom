2eb82c6919c7e75f24eb74433771c1765b92f6ca
louis1706 <louismonneyron5@yahoo.com>
2022-08-15 14:40:24 +0200

    Initial commit

---
+++ Config.cs
@@ -0,0 +1,16 @@
+﻿using Exiled.API.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomIntercom
+{
+    public class Config : IConfig
+    {
+        public bool IsEnabled { get; set; } = false;
+
+        public string CustomText { get; set; } = string.Empty;
+    }
+}
